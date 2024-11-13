152af80484ea152d664ae113c979f02f900392c4
Anton Pushkarev <a.pushkarev@kassir.ru>
2024-11-13 20:32:16 +0400

    chore break line

--- app.vue
+++ app.vue
@@ -5,6 +5,7 @@
       href="bank100000000111://qr.nspk.ru/AD20005APGJ17D4H8T7ORJIIUBVJF35Q?type=02&bank=100000000004&sum=210000&cur=RUB&crc=C50C"
       >Iphone Сбербанк</a
     >
+    <br />
     <a
       target="_top"
       href="intent://qr.nspk.ru/BD2000566BJPNT1N8LAAO16J1LJT2SEB?type=02&bank=100000000004&sum=121000&cur=RUB&crc=97EC#Intent;scheme=bank100000000111;end"
